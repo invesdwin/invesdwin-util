5e0f5098f5f8465064151adb8da6ad358c13f61a
subes <gsubes@gmail.com>
2015-07-22 22:33:00 +0200

    datatype converter test moved

---
+++ invesdwin-util/src/test/java/de/invesdwin/util/internal/jaxb/DatatypeConverterTest.java
@@ -0,0 +1,23 @@
+package de.invesdwin.util.internal.jaxb;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import org.junit.Test;
+
+import de.invesdwin.util.assertions.Assertions;
+import de.invesdwin.util.time.fdate.FDate;
+
+@NotThreadSafe
+public class DatatypeConverterTest {
+
+    @Test
+    public void testDateTime() {
+        final FDate date1 = new FDate();
+        final String str1 = DatatypeConverter.printDateTime(date1);
+        final FDate date2 = DatatypeConverter.parseDateTime(str1);
+        final String str2 = DatatypeConverter.printDateTime(date2);
+        Assertions.assertThat(date2).isEqualTo(date1);
+        Assertions.assertThat(str2).isEqualTo(str1);
+    }
+
+}
