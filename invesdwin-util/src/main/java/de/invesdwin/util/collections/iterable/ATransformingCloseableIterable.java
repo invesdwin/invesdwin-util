9595eafa894314c05f7065a9b3f6d9f7d46cdd1a
subes <gsubes@gmail.com>
2015-09-18 02:55:23 +0200

    nullpointer assertion

--- invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/ATransformingCloseableIterable.java
+++ invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/ATransformingCloseableIterable.java
@@ -8,6 +8,9 @@ public abstract class ATransformingCloseableIterable<S, R> implements ICloseable
     private final ICloseableIterable<? extends S> delegate;
 
     public ATransformingCloseableIterable(final ICloseableIterable<? extends S> delegate) {
+        if (delegate == null) {
+            throw new NullPointerException("delegate must not be null");
+        }
         this.delegate = delegate;
     }
 
