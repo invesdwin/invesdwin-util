579d4986c7789031caee6d8dcdaf6a1911f1da73
subes <gsubes@gmail.com>
2016-10-03 18:16:31 +0200

    we should also throw away caches when we find 1 new value, doesnt matter where it is found

--- invesdwin-util/src/main/java/de/invesdwin/util/collections/loadingcache/historical/query/internal/core/CachedHistoricalCacheQueryCore.java
+++ invesdwin-util/src/main/java/de/invesdwin/util/collections/loadingcache/historical/query/internal/core/CachedHistoricalCacheQueryCore.java
@@ -213,7 +213,7 @@ public class CachedHistoricalCacheQueryCore<V> implements IHistoricalCacheQueryC
     }
 
     private void replaceCachedEntries(final FDate adjKey, final List<Entry<FDate, V>> trailing) {
-        if (trailing.isEmpty() || (trailing.size() == 1 && cachedPreviousEntries.size() > 1)) {
+        if (trailing.isEmpty()) {
             //maybe we went before the first entry, so we don't throw away a cache that might already be filled
             return;
         }
