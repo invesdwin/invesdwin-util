0bedffe1ca7c97509412eb4064fb9fadb085f2ac
subes <gsubes@gmail.com>
2016-10-27 15:07:42 +0200

    Sets

---
+++ invesdwin-util/src/main/java/de/invesdwin/util/collections/Sets.java
@@ -0,0 +1,27 @@
+package de.invesdwin.util.collections;
+
+import java.util.Set;
+
+import javax.annotation.concurrent.Immutable;
+
+import de.invesdwin.norva.apt.staticfacade.StaticFacadeDefinition;
+import de.invesdwin.util.collections.internal.ASetsStaticFacade;
+
+@Immutable
+@StaticFacadeDefinition(name = "de.invesdwin.util.collections.internal.ASetsStaticFacade", targets = {
+        com.google.common.collect.Sets.class, org.apache.commons.collections.SetUtils.class })
+public final class Sets extends ASetsStaticFacade {
+
+    private Sets() {}
+
+    public static boolean equals(final Set<?> set1, final Set<?> set2) {
+        final boolean set1NullOrEmpty = set1 == null || set1.isEmpty();
+        final boolean set2NullOrEmpty = set2 == null || set2.isEmpty();
+        if (set1NullOrEmpty && set2NullOrEmpty) {
+            return true;
+        } else {
+            return isEqualSet(set1, set2);
+        }
+    }
+
+}
