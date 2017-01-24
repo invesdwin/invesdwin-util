c8de4693c363a5b55b4734c7d446351f34248e77
subes <gsubes@gmail.com>
2017-01-24 22:22:40 +0100

    median

---
+++ invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/ADecimalMedian.java
@@ -0,0 +1,29 @@
+package de.invesdwin.util.math.decimal;
+
+import java.util.List;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * http://stackoverflow.com/questions/11955728/how-to-calculate-the-median-of-an-array
+ */
+@Immutable
+public abstract class ADecimalMedian<E extends ADecimal<E>> {
+
+    protected abstract List<E> getSortedList();
+
+    public E getMedian() {
+        final List<E> sortedList = getSortedList();
+        final int middle = sortedList.size() / 2;
+        final E median;
+        if (sortedList.size() % 2 == 0) {
+            final E medianA = sortedList.get(middle);
+            final E medianB = sortedList.get(middle - 1);
+            median = medianA.add(medianB).divide(2);
+        } else {
+            median = sortedList.get(middle + 1);
+        }
+        return median;
+    }
+
+}
