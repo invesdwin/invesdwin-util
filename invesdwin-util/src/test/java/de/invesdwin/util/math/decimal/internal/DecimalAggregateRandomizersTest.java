06fb7015d8446400bee1384b2e549105e3c903f4
subes <gsubes@gmail.com>
2017-02-03 15:17:39 +0100

    -shuffle

--- invesdwin-util/src/test/java/de/invesdwin/util/math/decimal/internal/DecimalAggregateRandomizersTest.java
+++ invesdwin-util/src/test/java/de/invesdwin/util/math/decimal/internal/DecimalAggregateRandomizersTest.java
@@ -19,62 +19,63 @@ public class DecimalAggregateRandomizersTest {
     private static final int REPEAT_COUNT = 100;
 
     @Test
-        public void testRandomizeShuffle() {
-            final List<Decimal> values = new ArrayList<Decimal>();
-            for (int i = 0; i < 100000; i++) {
-                values.add(new Decimal(i));
-            }
-    
-            final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
-            for (int i = 0; i < REPEAT_COUNT; i++) {
-                final List<? extends Decimal> resampled = Lists.toList(agg.randomizeShuffle(RandomGenerators.newDefaultRandom()));
-                Assertions.assertThat(values).hasSameSizeAs(resampled);
-            }
+    public void testRandomizeShuffle() {
+        final List<Decimal> values = new ArrayList<Decimal>();
+        for (int i = 0; i < 100000; i++) {
+            values.add(new Decimal(i));
         }
 
-    @Test
-        public void testRandomizeShuffleBootstrap() {
-            final List<Decimal> values = new ArrayList<Decimal>();
-            for (int i = 0; i < 100000; i++) {
-                values.add(new Decimal(i));
-            }
-    
-            final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
-            for (int i = 0; i < REPEAT_COUNT; i++) {
-                final List<? extends Decimal> resampled = Lists
-                        .toList(agg.randomizeBootstrap(RandomGenerators.newDefaultRandom()));
-                Assertions.assertThat(values).hasSameSizeAs(resampled);
-            }
+        final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
+        for (int i = 0; i < REPEAT_COUNT; i++) {
+            final List<? extends Decimal> resampled = Lists
+                    .toList(agg.randomizeShuffle(RandomGenerators.newDefaultRandom()));
+            Assertions.assertThat(values).hasSameSizeAs(resampled);
         }
+    }
 
     @Test
-        public void testRandomizeShuffleCircularBlockBootstrap() {
-            final List<Decimal> values = new ArrayList<Decimal>();
-            for (int i = 0; i < 100000; i++) {
-                values.add(new Decimal(i));
-            }
-    
-            final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
-            for (int i = 0; i < REPEAT_COUNT; i++) {
-                final List<? extends Decimal> resampled = Lists
-                        .toList(agg.randomizeCircularBlockBootstrap(RandomGenerators.newDefaultRandom()));
-                Assertions.assertThat(values).hasSameSizeAs(resampled);
-            }
+    public void testRandomizeBootstrap() {
+        final List<Decimal> values = new ArrayList<Decimal>();
+        for (int i = 0; i < 100000; i++) {
+            values.add(new Decimal(i));
         }
 
-    @Test
-        public void testRandomizeShuffleStationaryBootstrap() {
-            final List<Decimal> values = new ArrayList<Decimal>();
-            for (int i = 0; i < 100000; i++) {
-                values.add(new Decimal(i));
-            }
-    
-            final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
-            for (int i = 0; i < REPEAT_COUNT; i++) {
-                final List<? extends Decimal> resampled = Lists
-                        .toList(agg.randomizeStationaryBootstrap(RandomGenerators.newDefaultRandom()));
-                Assertions.assertThat(values).hasSameSizeAs(resampled);
-            }
+        final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
+        for (int i = 0; i < REPEAT_COUNT; i++) {
+            final List<? extends Decimal> resampled = Lists
+                    .toList(agg.randomizeBootstrap(RandomGenerators.newDefaultRandom()));
+            Assertions.assertThat(values).hasSameSizeAs(resampled);
         }
+    }
+
+    @Test
+    public void testRandomizeCircularBlockBootstrap() {
+        final List<Decimal> values = new ArrayList<Decimal>();
+        for (int i = 0; i < 100000; i++) {
+            values.add(new Decimal(i));
+        }
+
+        final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
+        for (int i = 0; i < REPEAT_COUNT; i++) {
+            final List<? extends Decimal> resampled = Lists
+                    .toList(agg.randomizeCircularBlockBootstrap(RandomGenerators.newDefaultRandom()));
+            Assertions.assertThat(values).hasSameSizeAs(resampled);
+        }
+    }
+
+    @Test
+    public void testRandomizeStationaryBootstrap() {
+        final List<Decimal> values = new ArrayList<Decimal>();
+        for (int i = 0; i < 100000; i++) {
+            values.add(new Decimal(i));
+        }
+
+        final IDecimalAggregate<Decimal> agg = Decimal.valueOf(values);
+        for (int i = 0; i < REPEAT_COUNT; i++) {
+            final List<? extends Decimal> resampled = Lists
+                    .toList(agg.randomizeStationaryBootstrap(RandomGenerators.newDefaultRandom()));
+            Assertions.assertThat(values).hasSameSizeAs(resampled);
+        }
+    }
 
 }
