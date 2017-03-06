255d40e180920705c252cd3df568ac79c61770a0
subes <gsubes@gmail.com>
2017-03-06 18:38:00 +0100

    allowing different measures in whites reality check

---
+++ invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/stream/DecimalStreamProfitFactor.java
@@ -0,0 +1,30 @@
+package de.invesdwin.util.math.decimal.stream;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import de.invesdwin.util.math.decimal.ADecimal;
+import de.invesdwin.util.math.decimal.Decimal;
+import de.invesdwin.util.math.decimal.scaled.Percent;
+
+@NotThreadSafe
+public class DecimalStreamProfitFactor<E extends ADecimal<E>> implements IDecimalStreamAlgorithm<E, Void> {
+
+    private double profitSum;
+    private double lossSum;
+
+    @Override
+    public Void process(final E value) {
+        final double doubleValue = value.doubleValueRaw();
+        if (doubleValue > 0) {
+            profitSum += doubleValue;
+        } else {
+            lossSum += doubleValue;
+        }
+        return null;
+    }
+
+    public Percent getProfitFactor() {
+        return new Percent(new Decimal(profitSum), new Decimal(lossSum));
+    }
+
+}
