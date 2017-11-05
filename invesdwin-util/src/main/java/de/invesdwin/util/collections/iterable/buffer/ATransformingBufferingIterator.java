ded271152dcb2af8c6ddb4be00f89a784014945b
subes <gsubes@gmail.com>
2017-11-05 19:00:01 +0100

    transforming buffering iterator

---
+++ invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/buffer/ATransformingBufferingIterator.java
@@ -0,0 +1,124 @@
+package de.invesdwin.util.collections.iterable.buffer;
+
+import java.util.Iterator;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import de.invesdwin.util.collections.iterable.ATransformingCloseableIterator;
+import de.invesdwin.util.collections.iterable.ICloseableIterator;
+
+@NotThreadSafe
+public abstract class ATransformingBufferingIterator<S, R> implements IBufferingIterator<R> {
+
+    private final IBufferingIterator<? extends S> delegate;
+
+    public ATransformingBufferingIterator(final IBufferingIterator<? extends S> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return delegate.hasNext();
+    }
+
+    @Override
+    public R next() {
+        return null;
+    }
+
+    @Override
+    public ICloseableIterator<R> iterator() {
+        return new ATransformingCloseableIterator<S, R>(delegate.iterator()) {
+            @Override
+            protected R transform(final S value) {
+                return ATransformingBufferingIterator.this.transform(value);
+            }
+        };
+    }
+
+    protected abstract R transform(S value);
+
+    @Override
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    @Override
+    public R getHead() {
+        final S head = delegate.getHead();
+        if (head == null) {
+            return null;
+        }
+        return transform(head);
+    }
+
+    @Override
+    public R getTail() {
+        final S tail = delegate.getTail();
+        if (tail == null) {
+            return null;
+        }
+        return transform(tail);
+    }
+
+    @Deprecated
+    @Override
+    public void prepend(final R element) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+    @Deprecated
+    @Override
+    public void add(final R element) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+    @Deprecated
+    @Override
+    public boolean addAll(final Iterator<? extends R> iterator) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+    @Deprecated
+    @Override
+    public boolean addAll(final Iterable<? extends R> iterable) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+    @Deprecated
+    @Override
+    public boolean addAll(final BufferingIterator<R> iterable) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+    @Override
+    public void clear() {
+        delegate.clear();
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public boolean consume(final Iterable<? extends R> iterable) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+    @Override
+    public boolean consume(final Iterator<? extends R> iterator) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+    @Override
+    public boolean consume(final BufferingIterator<R> iterator) {
+        throw new UnsupportedOperationException("can not add");
+    }
+
+}
