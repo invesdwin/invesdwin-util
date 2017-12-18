d6c2a8c77ef858caea245bd1f21369f23b4e7139
subes <gsubes@gmail.com>
2017-12-18 07:14:21 +0100

    buffering retrieval

---
+++ invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/buffer/ABufferingRetrievalCloseableIterable.java
@@ -0,0 +1,74 @@
+package de.invesdwin.util.collections.iterable.buffer;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import de.invesdwin.util.collections.iterable.ICloseableIterable;
+import de.invesdwin.util.collections.iterable.ICloseableIterator;
+import de.invesdwin.util.error.FastNoSuchElementException;
+import de.invesdwin.util.time.fdate.FDate;
+
+@NotThreadSafe
+public abstract class ABufferingRetrievalCloseableIterable<T> implements ICloseableIterable<T> {
+
+    private final FDate fromDate;
+    private final FDate toDate;
+    private final Integer retrievalCount;
+
+    public ABufferingRetrievalCloseableIterable(final FDate fromDate, final FDate toDate, final Integer retrievalCount) {
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.retrievalCount = retrievalCount;
+    }
+
+    @Override
+    public ICloseableIterator<T> iterator() {
+        return new ICloseableIterator<T>() {
+
+            private FDate curDate = fromDate;
+            private IBufferingIterator<? extends T> curList = queryNext(curDate);
+
+            private IBufferingIterator<? extends T> getList() {
+                if (curList == null) {
+                    return null;
+                }
+                if (curList.isEmpty()) {
+                    curList = queryNext(curDate.addMilliseconds(1));
+                    if (curList.isEmpty()) {
+                        close();
+                    }
+                }
+                return curList;
+            }
+
+            private IBufferingIterator<? extends T> queryNext(final FDate fromDate) {
+                return query(fromDate, toDate, retrievalCount);
+            }
+
+            @Override
+            public boolean hasNext() {
+                final IBufferingIterator<? extends T> list = getList();
+                return list != null && !list.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                final IBufferingIterator<? extends T> list = getList();
+                if (list == null) {
+                    throw new FastNoSuchElementException("ARedirectingCloseableIterable: list is null");
+                }
+                final T next = list.next();
+                curDate = extractTime(next);
+                return next;
+            }
+
+            @Override
+            public void close() {
+                curList = null;
+            }
+        };
+    }
+
+    protected abstract FDate extractTime(T next);
+
+    protected abstract IBufferingIterator<? extends T> query(FDate fromDate, FDate toDate, Integer count);
+}
