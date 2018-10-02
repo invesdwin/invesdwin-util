0be9e834b7e9eb90f9bcf01200c159ac564520c6
subes <gsubes@gmail.com>
2018-10-02 22:01:27 +0200

    dont cache hashcode

--- invesdwin-util/src/main/java/de/invesdwin/util/time/fdate/FDate.java
+++ invesdwin-util/src/main/java/de/invesdwin/util/time/fdate/FDate.java
@@ -100,20 +100,16 @@ public class FDate implements IDate, Serializable, Cloneable, Comparable<Object>
     @Transient
     private transient Object extension;
 
-    private final int hashCode;
-
     public FDate() {
         this(System.currentTimeMillis());
     }
 
     public FDate(final long millis) {
         this.millis = millis;
-        this.hashCode = Long.hashCode(millis);
     }
 
     protected FDate(final FDate date) {
         this.millis = date.millis;
-        this.hashCode = date.hashCode;
     }
 
     public FDate(final ReadableDateTime jodaTime) {
@@ -520,7 +516,7 @@ public class FDate implements IDate, Serializable, Cloneable, Comparable<Object>
 
     @Override
     public int hashCode() {
-        return hashCode;
+        return Long.hashCode(millis);
     }
 
     @Override
