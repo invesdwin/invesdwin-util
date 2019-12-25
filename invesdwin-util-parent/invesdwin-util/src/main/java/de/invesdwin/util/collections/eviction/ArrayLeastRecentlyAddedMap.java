4929a2d67986d59ef494ccea876cd4333a859d96
subes <gsubes@gmail.com>
2019-12-25 23:21:06 +0100

    evict in groups

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/eviction/ArrayLeastRecentlyAddedMap.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/eviction/ArrayLeastRecentlyAddedMap.java
@@ -15,14 +15,17 @@ import de.invesdwin.util.math.Integers;
 @NotThreadSafe
 public class ArrayLeastRecentlyAddedMap<K, V> implements Map<K, V>, IEvictionMap<K, V> {
 
+    private static final int EVICTION_SIZE_MULTIPLIER = 2;
+
     private int maximumSize;
+    private int evictionSize;
     private Object[] orderedKeys;
     private int leastRecentlyAddedKeyIndex = -1;
     private int mostRecentlyAddedKeyIndex = -1;
     private final Map<K, V> map;
 
     public ArrayLeastRecentlyAddedMap(final int maximumSize) {
-        this.maximumSize = maximumSize;
+        setMaximumSize(maximumSize);
         this.orderedKeys = new Object[0];
         this.map = newMap();
     }
@@ -80,24 +83,26 @@ public class ArrayLeastRecentlyAddedMap<K, V> implements Map<K, V>, IEvictionMap
 
     private void updateMostRecentlyAdded(final K key) {
         mostRecentlyAddedKeyIndex++;
-        if (mostRecentlyAddedKeyIndex >= maximumSize) {
+        if (mostRecentlyAddedKeyIndex >= evictionSize) {
             mostRecentlyAddedKeyIndex = 0;
         }
         if (orderedKeys.length <= mostRecentlyAddedKeyIndex) {
             final Object[] oldArray = orderedKeys;
-            orderedKeys = new Object[Integers.max(1, Integers.min(maximumSize, oldArray.length * 2))];
+            orderedKeys = new Object[Integers.max(1, Integers.min(evictionSize, oldArray.length * 2))];
             System.arraycopy(oldArray, 0, orderedKeys, 0, oldArray.length);
         }
-        while (map.size() > maximumSize) {
-            do {
-                leastRecentlyAddedKeyIndex++;
-                if (leastRecentlyAddedKeyIndex >= maximumSize) {
-                    leastRecentlyAddedKeyIndex = 0;
-                }
-                //jump over removed keys
-            } while (orderedKeys[leastRecentlyAddedKeyIndex] == null);
-            map.remove(orderedKeys[leastRecentlyAddedKeyIndex]);
-            orderedKeys[leastRecentlyAddedKeyIndex] = null;
+        if (map.size() > evictionSize) {
+            while (map.size() > maximumSize) {
+                do {
+                    leastRecentlyAddedKeyIndex++;
+                    if (leastRecentlyAddedKeyIndex >= evictionSize) {
+                        leastRecentlyAddedKeyIndex = 0;
+                    }
+                    //jump over removed keys
+                } while (orderedKeys[leastRecentlyAddedKeyIndex] == null);
+                map.remove(orderedKeys[leastRecentlyAddedKeyIndex]);
+                orderedKeys[leastRecentlyAddedKeyIndex] = null;
+            }
         }
         orderedKeys[mostRecentlyAddedKeyIndex] = key;
     }
@@ -167,6 +172,7 @@ public class ArrayLeastRecentlyAddedMap<K, V> implements Map<K, V>, IEvictionMap
     @Override
     public void setMaximumSize(final int maximumSize) {
         this.maximumSize = maximumSize;
+        this.evictionSize = maximumSize * EVICTION_SIZE_MULTIPLIER;
     }
 
     @Override
