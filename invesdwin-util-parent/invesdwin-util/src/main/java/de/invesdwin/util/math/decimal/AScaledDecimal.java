0086d0a35081f9cbcce8632e9546e23de3ca5738
subes <gsubes@gmail.com>
2020-01-17 20:30:27 +0100

    assert again

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/AScaledDecimal.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/AScaledDecimal.java
@@ -223,7 +223,28 @@ public abstract class AScaledDecimal<T extends AScaledDecimal<T, S>, S extends I
     }
 
     private double maybeGetDefaultScaledNumber(final ADecimal<?> number) {
+        assertSameDefaultScale(number);
         return number.getDefaultValue();
+    }
+
+    public void assertSameDefaultScale(final Number number) {
+        assert isSameDefaultScale(number) : newSameDefaultScaleErrorMessage(number);
+    }
+
+    private String newSameDefaultScaleErrorMessage(final Number number) {
+        final AScaledDecimal<?, ?> scaledNumber = (AScaledDecimal<?, ?>) number;
+        return "Cannot mix two different default scales on division: " + getDefaultScale() + " [" + this + "]  != "
+                + scaledNumber.getDefaultScale() + " [" + scaledNumber + "]";
+    }
+
+    private boolean isSameDefaultScale(final Number number) {
+        if (number instanceof AScaledDecimal) {
+            final AScaledDecimal<?, ?> scaledNumber = (AScaledDecimal<?, ?>) number;
+            if (!scaledNumber.getDefaultScale().equals(getDefaultScale())) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static <T, D extends ADecimal<D>> List<D> extractValues(final Function<T, D> getter, final List<T> objects) {
