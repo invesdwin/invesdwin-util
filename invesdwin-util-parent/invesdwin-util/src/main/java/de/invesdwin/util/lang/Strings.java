da5711d7bedfa0a6175a2a765ae94af70ca12058
subes <gsubes@gmail.com>
2020-01-18 02:18:34 +0100

    wordutil

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Strings.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Strings.java
@@ -21,7 +21,8 @@ import de.invesdwin.util.lang.internal.MultilineToStringStyle;
 @Immutable
 @StaticFacadeDefinition(name = "de.invesdwin.util.lang.internal.AStringsStaticFacade", targets = {
         CheckedCastStrings.class, BeanPathStrings.class, com.google.common.base.Strings.class,
-        org.assertj.core.util.Strings.class }, filterMethodSignatureExpressions = {
+        org.assertj.core.util.Strings.class, org.apache.commons.text.StringEscapeUtils.class,
+        org.apache.commons.text.WordUtils.class }, filterMethodSignatureExpressions = {
                 ".* java\\.lang\\.String repeat\\(java\\.lang\\.String str, int repeat\\).*", ".* isNullOrEmpty\\(.*" })
 public final class Strings extends AStringsStaticFacade {
 
