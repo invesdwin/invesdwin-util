e1d3caf9b2e139b05c802ae03afa9dc6c6c80e2a
subes <gsubes@gmail.com>
2020-03-15 10:02:15 +0100

    lower maximum size increase to debug

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/loadingcache/historical/AHistoricalCache.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/loadingcache/historical/AHistoricalCache.java
@@ -123,8 +123,8 @@ public abstract class AHistoricalCache<V>
     protected void innerIncreaseMaximumSize(final int maximumSize, final String reason) {
         queryCore.increaseMaximumSize(maximumSize);
         if (isDebugAutomaticReoptimization() || maximumSize >= getMaximumSizeLimit()) {
-            if (getMaximumSize() != null && getMaximumSize() > 0 && LOG.isWarnEnabled()) {
-                LOG.warn(this + ": Increasing maximum size from [" + this.maximumSize + "] to [" + maximumSize
+            if (getMaximumSize() != null && getMaximumSize() > 0 && LOG.isDebugEnabled()) {
+                LOG.debug(this + ": Increasing maximum size from [" + this.maximumSize + "] to [" + maximumSize
                         + "] with reason [" + reason + "]");
             }
         }
