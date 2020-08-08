12073bfe170903ee31702ea468b3bdb179ac2f8d
subes <gsubes@gmail.com>
2020-08-08 23:13:56 +0200

    substringBetweenLast

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Strings.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Strings.java
@@ -601,4 +601,18 @@ public final class Strings extends AStringsStaticFacade {
         return replace(space, " ", "");
     }
 
+    public static String substringBetweenLast(final String str, final String open, final String close) {
+        if (str == null || open == null || close == null) {
+            return null;
+        }
+        final int start = str.lastIndexOf(open);
+        if (start != -1) {
+            final int end = str.indexOf(close, start + open.length());
+            if (end != -1) {
+                return str.substring(start + open.length(), end);
+            }
+        }
+        return null;
+    }
+
 }
