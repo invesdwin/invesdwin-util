4fc233cc0d7bc5e7811437a88ab563b22c5bba95
subes <gsubes@gmail.com>
2020-09-19 10:59:44 +0200

    empty list

---
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/EmptyList.java
@@ -0,0 +1,138 @@
+package de.invesdwin.util.collections;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.annotation.concurrent.Immutable;
+
+import de.invesdwin.util.collections.iterable.EmptyCloseableIterator;
+
+@Immutable
+public final class EmptyList<E> implements List<E> {
+
+    private static final EmptyList INSTANCE = new EmptyList<>();
+
+    private EmptyList() {
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return EmptyCloseableIterator.getInstance();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(final T[] a) {
+        return a;
+    }
+
+    @Override
+    public boolean add(final E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(final int index, final Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+    }
+
+    @Override
+    public E get(final int index) {
+        return null;
+    }
+
+    @Override
+    public E set(final int index, final E element) {
+        return null;
+    }
+
+    @Override
+    public void add(final int index, final E element) {
+    }
+
+    @Override
+    public E remove(final int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(final Object o) {
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(final Object o) {
+        return -1;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return Collections.emptyListIterator();
+    }
+
+    @Override
+    public ListIterator<E> listIterator(final int index) {
+        return Collections.emptyListIterator();
+    }
+
+    @Override
+    public List<E> subList(final int fromIndex, final int toIndex) {
+        return this;
+    }
+
+    public static <T> EmptyList<T> getInstance() {
+        return INSTANCE;
+    }
+
+}
