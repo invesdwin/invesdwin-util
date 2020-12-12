bf4a6fabaee4968ecfd686400c89f1a6511c3cb1
subes <gsubes@gmail.com>
2020-12-12 12:00:36 +0100

    bytebuffer and snapshot fixes

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/stream/doubl/correlation/ICorrelation.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/stream/doubl/correlation/ICorrelation.java
@@ -12,9 +12,16 @@ public interface ICorrelation {
      * CorrCoef: Correlation Coefficient is a measure of how linear an equity curve is. If it jumps around or is
      * non-linear in shape then this value will be low, but if it progress with a slope close to 1 then the value shall
      * be close to 1. The higher this value the more linear the account growth or equity curve is.
+     * 
+     * We use the correlation positive/negative sign also for the coefficient of determination.
      */
     default Percent getCoefficientOfDetermination() {
-        return getCorrelation().square();
+        final Percent correlation = getCorrelation();
+        if (correlation.isNegative()) {
+            return correlation.square().negate();
+        } else {
+            return correlation.square();
+        }
     }
 
     default CorrelationType getCorrelationType() {
