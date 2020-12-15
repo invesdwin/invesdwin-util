c6d7876234d2efb7f402d799af42c4125ec78917
subes <gsubes@gmail.com>
2020-12-15 20:58:48 +0100

    fix loop

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/time/fdate/LoopIsSameJulianDayCheck.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/time/fdate/LoopIsSameJulianDayCheck.java
@@ -6,31 +6,41 @@ import javax.annotation.concurrent.NotThreadSafe;
 public class LoopIsSameJulianDayCheck {
 
     private long prevJulianDay;
+    private FDate lastTime;
 
     public LoopIsSameJulianDayCheck() {
         this.prevJulianDay = 0L;
+        this.lastTime = FDate.MIN_DATE;
     }
 
     public LoopIsSameJulianDayCheck(final FDate initialTime) {
         this.prevJulianDay = initialTime.millisValue() / FDates.MILLISECONDS_IN_DAY;
+        this.lastTime = initialTime;
+    }
+
+    public FDate getLastTime() {
+        return lastTime;
     }
 
     public boolean isSameJulianDay(final FDate time) {
         final long newJulianDay = time.millisValue() / FDates.MILLISECONDS_IN_DAY;
-        if (prevJulianDay != newJulianDay) {
-            prevJulianDay = newJulianDay;
+        if (prevJulianDay == newJulianDay) {
             return true;
         } else {
+            lastTime = time;
+            prevJulianDay = newJulianDay;
             return false;
         }
     }
 
     public void reset() {
         prevJulianDay = 0L;
+        lastTime = FDate.MIN_DATE;
     }
 
     public void reset(final FDate initialTime) {
         prevJulianDay = initialTime.millisValue() / FDates.MILLISECONDS_IN_DAY;
+        lastTime = initialTime;
     }
 
 }
