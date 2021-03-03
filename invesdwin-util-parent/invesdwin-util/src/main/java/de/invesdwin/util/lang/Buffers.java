8793b3c72aa855ab5d3077388a7b4f5e2ac6fc07
subes <gsubes@gmail.com>
2021-03-03 10:32:28 +0100

    compile fix

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Buffers.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Buffers.java
@@ -1,6 +1,7 @@
 package de.invesdwin.util.lang;
 
 import java.nio.Buffer;
+import java.nio.ByteBuffer;
 
 import javax.annotation.concurrent.Immutable;
 
@@ -15,6 +16,28 @@ public final class Buffers {
      */
     public static void position(final Buffer buffer, final int position) {
         buffer.position(position);
+    }
+
+    public static void get(final ByteBuffer buffer, final int position, final byte[] dst) {
+        final ByteBuffer duplicate = buffer.duplicate();
+        Buffers.position(duplicate, position);
+        duplicate.get(dst);
+    }
+
+    public static byte[] getRemaining(final ByteBuffer buffer, final int position) {
+        final ByteBuffer duplicate = buffer.duplicate();
+        Buffers.position(duplicate, position);
+        final byte[] dst = new byte[duplicate.remaining()];
+        duplicate.get(dst);
+        return dst;
+    }
+
+    public static byte[] get(final ByteBuffer buffer, final int position, final int size) {
+        final ByteBuffer duplicate = buffer.duplicate();
+        Buffers.position(duplicate, position);
+        final byte[] dst = new byte[size];
+        duplicate.get(dst);
+        return dst;
     }
 
 }
