1334179923560e97974ffb083f87522cc8b5a9c8
subes <gsubes@gmail.com>
2021-03-16 23:11:04 +0100

    exc name

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/assertions/type/internal/junit/JUnit4CheckEquals.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/assertions/type/internal/junit/JUnit4CheckEquals.java
@@ -9,6 +9,20 @@ import de.invesdwin.util.lang.description.TextDescription;
 @Immutable
 public final class JUnit4CheckEquals {
 
+    private static final class ComparisonFailure extends org.junit.ComparisonFailure {
+        private final String abbreviatedMessage;
+
+        private ComparisonFailure(final String message, final String expected, final String actual) {
+            super(message, expected, actual);
+            this.abbreviatedMessage = message;
+        }
+
+        @Override
+        public String getMessage() {
+            return abbreviatedMessage;
+        }
+    }
+
     private static final int COMPARISON_FAILURE_MESSAGE_LIMIT = Assertions.COMPARISON_FAILURE_MESSAGE_LIMIT;
 
     private JUnit4CheckEquals() {
@@ -20,12 +34,7 @@ public final class JUnit4CheckEquals {
             org.junit.Assert.assertEquals(TextDescription.format(message, args), expected, actual);
         } catch (final org.junit.ComparisonFailure e) {
             final String abbreviatedMessage = Strings.abbreviate(e.getMessage(), COMPARISON_FAILURE_MESSAGE_LIMIT);
-            throw new org.junit.ComparisonFailure(abbreviatedMessage, e.getExpected(), e.getActual()) {
-                @Override
-                public String getMessage() {
-                    return abbreviatedMessage;
-                }
-            };
+            throw new ComparisonFailure(abbreviatedMessage, e.getExpected(), e.getActual());
         }
     }
 
@@ -35,12 +44,7 @@ public final class JUnit4CheckEquals {
         } catch (final org.junit.ComparisonFailure e) {
             //limit message length or else eclipse freezes in junit dialog
             final String abbreviatedMessage = Strings.abbreviate(e.getMessage(), COMPARISON_FAILURE_MESSAGE_LIMIT);
-            throw new org.junit.ComparisonFailure(abbreviatedMessage, e.getExpected(), e.getActual()) {
-                @Override
-                public String getMessage() {
-                    return abbreviatedMessage;
-                }
-            };
+            throw new ComparisonFailure(abbreviatedMessage, e.getExpected(), e.getActual());
         }
     }
 
