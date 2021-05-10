c596694b5255e36b5cc7b23c02633c117dce94df
subes <gsubes@gmail.com>
2021-05-10 15:21:54 +0200

    supplier

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/AttributesMap.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/AttributesMap.java
@@ -3,7 +3,8 @@ package de.invesdwin.util.collections;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Callable;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -25,8 +26,36 @@ public class AttributesMap extends ADelegateMap<String, Object> {
         }
     }
 
+    /**
+     * Use getOrCreate instead
+     */
+    @Deprecated
+    @Override
+    public Object computeIfAbsent(final String key, final Function<? super String, ? extends Object> mappingFunction) {
+        Object v;
+        final Map<String, Object> delegate = getDelegate();
+        synchronized (delegate) {
+            v = delegate.get(key);
+        }
+        if (v == null) {
+            //bad idea to synchronize in apply, this might cause deadlocks when threads are used inside of it
+            v = mappingFunction.apply(key);
+            if (v != null) {
+                synchronized (delegate) {
+                    final Object oldV = delegate.get(key);
+                    if (oldV != null) {
+                        v = oldV;
+                    } else {
+                        delegate.put(key, v);
+                    }
+                }
+            }
+        }
+        return v;
+    }
+
     @SuppressWarnings("unchecked")
-    public <T> T getOrCreate(final String key, final Callable<T> createCallable) {
+    public <T> T getOrCreate(final String key, final Supplier<T> createSupplier) {
         T v;
         final Map<String, Object> delegate = getDelegate();
         synchronized (delegate) {
@@ -34,11 +63,7 @@ public class AttributesMap extends ADelegateMap<String, Object> {
         }
         if (v == null) {
             //bad idea to synchronize in apply, this might cause deadlocks when threads are used inside of it
-            try {
-                v = createCallable.call();
-            } catch (final Exception e) {
-                throw new RuntimeException(e);
-            }
+            v = createSupplier.get();
             if (v != null) {
                 synchronized (delegate) {
                     final T oldV = (T) delegate.get(key);
