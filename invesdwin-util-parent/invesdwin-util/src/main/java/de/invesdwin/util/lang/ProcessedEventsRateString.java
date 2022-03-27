f2711bbc25d4e488c4c56024c17544de223e34ad
Edwin Stang <gsubes@gmail.com>
2022-03-27 01:41:19 +0100

    precise calculations

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/ProcessedEventsRateString.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/ProcessedEventsRateString.java
@@ -34,20 +34,17 @@ public class ProcessedEventsRateString {
         }
         final double ratePerMillisecond = countEvents / milliseconds;
         if (ratePerMillisecond < 10 && duration.isGreaterThan(Duration.ONE_SECOND)) {
-            final double seconds = duration.doubleValue(FTimeUnit.SECONDS);
-            final double ratePerSecond = countEvents / seconds;
-            final double ratePerSecondPrecise = ratePerMillisecond * FTimeUnit.MILLISECONDS_IN_SECOND;
+            final double ratePerSecond = ratePerMillisecond * FTimeUnit.MILLISECONDS_IN_SECOND;
             if (ratePerSecond < 1) {
                 final double ratePerMinute = ratePerSecond * FTimeUnit.SECONDS_IN_MINUTE;
-                final double ratePerMinutePrecise = ratePerSecondPrecise * FTimeUnit.SECONDS_IN_MINUTE;
                 if (ratePerMinute < 1) {
-                    final double ratePerHourPrecise = ratePerMinutePrecise * FTimeUnit.MINUTES_IN_HOUR;
-                    return new Decimal(ratePerHourPrecise).round(2) + "/h";
+                    final double ratePerHour = ratePerMinute * FTimeUnit.MINUTES_IN_HOUR;
+                    return new Decimal(ratePerHour).round(2) + "/h";
                 } else {
-                    return new Decimal(ratePerMinutePrecise).round(2) + "/m";
+                    return new Decimal(ratePerMinute).round(2) + "/m";
                 }
             } else {
-                return new Decimal(ratePerSecondPrecise).round(2) + "/s";
+                return new Decimal(ratePerSecond).round(2) + "/s";
             }
         } else {
             if (ratePerMillisecond > 10_000) {
