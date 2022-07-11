931f902f873078d95c5ed102e6e1fc093ac0e1ca
Edwin Stang <gsubes@gmail.com>
2022-07-11 16:47:46 +0200

    PER-8 prepare rotating colors for positions

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Colors.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/Colors.java
@@ -83,4 +83,23 @@ public final class Colors {
         return new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
     }
 
+    public static Color getContrastColor(final Color color) {
+        return getContrastColor(color, Color.black, Color.white);
+    }
+
+    /**
+     * https://stackoverflow.com/questions/4672271/reverse-opposing-colors
+     */
+    public static Color getContrastColor(final Color color, final Color dark, final Color bright) {
+        final double y = (299 * color.getRed() + 587 * color.getGreen() + 114 * color.getBlue()) / 1000;
+        return y >= 128 ? dark : bright;
+    }
+
+    public static int getComplementaryColor(final Color colorToInvert) {
+        final float[] hsb = new float[3];
+        Color.RGBtoHSB(colorToInvert.getRed(), colorToInvert.getGreen(), colorToInvert.getBlue(), hsb);
+        hsb[0] = (hsb[0] + 180) % 360;
+        return Color.HSBtoRGB(hsb[0], hsb[1], hsb[2]);
+    }
+
 }
