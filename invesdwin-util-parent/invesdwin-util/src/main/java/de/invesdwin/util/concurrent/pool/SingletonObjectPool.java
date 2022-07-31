8beb1ea4839aa1d3342b5b22fc22ed9c6e7a1e87
Edwin Stang <gsubes@gmail.com>
2022-07-31 20:40:00 +0200

    singletonObjectPool

---
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/pool/SingletonObjectPool.java
@@ -0,0 +1,31 @@
+package de.invesdwin.util.concurrent.pool;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public class SingletonObjectPool<E> implements IObjectPool<E> {
+
+    private final E singleton;
+
+    public SingletonObjectPool(final E singleton) {
+        this.singleton = singleton;
+    }
+
+    @Override
+    public E borrowObject() {
+        return singleton;
+    }
+
+    @Override
+    public void returnObject(final E element) {
+    }
+
+    @Override
+    public void clear() {
+    }
+
+    @Override
+    public void invalidateObject(final E element) {
+    }
+
+}
