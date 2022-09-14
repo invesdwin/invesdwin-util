6f47f23a17efc321a770aa011f1ca784eb295061
Edwin Stang <gsubes@gmail.com>
2022-09-15 00:00:31 +0200

    PER-240 reverseIterable

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/IReverseCloseableIterable.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/IReverseCloseableIterable.java
@@ -4,4 +4,13 @@ public interface IReverseCloseableIterable<E> extends ICloseableIterable<E> {
 
     ICloseableIterator<E> reverseIterator();
 
+    default ICloseableIterable<E> reverseIterable() {
+        return new ICloseableIterable<E>() {
+            @Override
+            public ICloseableIterator<E> iterator() {
+                return reverseIterator();
+            }
+        };
+    }
+
 }
