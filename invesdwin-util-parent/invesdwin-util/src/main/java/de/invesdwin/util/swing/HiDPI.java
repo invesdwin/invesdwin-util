43b6b2a9b258bbb1e5d368fb42ff2e8b501f8a23
Edwin Stang <gsubes@gmail.com>
2022-10-18 01:00:07 +0200

    hidpi fixes

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/swing/HiDPI.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/swing/HiDPI.java
@@ -40,25 +40,51 @@ public final class HiDPI {
         return Fonts.resizeFont(font, scale(font.getSize()));
     }
 
+    public static Font descale(final Font font) {
+        return Fonts.resizeFont(font, descale(font.getSize()));
+    }
+
     public static int scale(final int size) {
         return (int) Math.ceil(size * scaleFactor);
+    }
+
+    public static int descale(final int size) {
+        return (int) Math.ceil(size / scaleFactor);
     }
 
     public static double scale(final double size) {
         return size * scaleFactor;
     }
 
+    public static double descale(final double size) {
+        return size / scaleFactor;
+    }
+
     public static float scale(final float size) {
         return size * scaleFactorFloat;
+    }
+
+    public static float descale(final float size) {
+        return size / scaleFactorFloat;
     }
 
     public static Dimension scale(final Dimension dimension) {
         return new Dimension(scale(dimension.width), scale(dimension.height));
     }
 
+    public static Dimension descale(final Dimension dimension) {
+        return new Dimension(descale(dimension.width), descale(dimension.height));
+    }
+
     public static ImageIcon scale(final ImageIcon icon) {
         return new ImageIcon(icon.getImage()
                 .getScaledInstance(scale(icon.getIconWidth()), scale(icon.getIconHeight()),
+                        java.awt.Image.SCALE_SMOOTH));
+    }
+
+    public static ImageIcon descale(final ImageIcon icon) {
+        return new ImageIcon(icon.getImage()
+                .getScaledInstance(descale(icon.getIconWidth()), descale(icon.getIconHeight()),
                         java.awt.Image.SCALE_SMOOTH));
     }
 
