cec3613afd69adf808d9ceb1ee7e74fc01214eab
matztejankow <m.jankow@1-2-3-invest.de>
2022-11-11 10:27:03 +0100

    PER-223: configurable hotkeys in PositionManager-Settings.

---
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/bean/APropertyChangeSupportedBase.java
@@ -0,0 +1,264 @@
+package de.invesdwin.util.bean;
+
+import java.beans.IndexedPropertyChangeEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import de.invesdwin.norva.beanpath.annotation.Hidden;
+import de.invesdwin.util.lang.Objects;
+import de.invesdwin.util.lang.reflection.Reflections;
+
+/**
+ * Standard implementation of {@link PropertyChangeSupported}. This class is thread-safe and uses a lazily initialized
+ * {@link PropertyChangeSupport} instance to do the heavy lifting. Initialization happens in either of the
+ * <code>addPropertyChangeListener</code> methods the first time a listener is added.
+ * <p>
+ * Implementing classes can safely declare serializability; implementors should be aware that the enclosed
+ * <code>PropertyChangeSupport</code> is marked <code>transient</code>.
+ * <p>
+ * No effort is made to unload the <code>PropertyChangeSupport</code> after the last listener unsubscribes.
+ * 
+ * Taken from: http://java.sogeti.nl/JavaBlog/2008/03/12/lazily-initialized-propertychangesupport/
+ * 
+ * @author Barend Garvelink
+ * @since 1.0
+ */
+@SuppressWarnings("restriction")
+@ThreadSafe
+public abstract class APropertyChangeSupportedBase {
+
+    private static final long PROPERTYCHANGESUPPORT_MAP_FIELD_OFFSET;
+    private static final long PROPERTYCHANGELISTENERMAP_MAP_FIELD_OFFSET;
+
+    static {
+        final Field mapField = Reflections.findField(PropertyChangeSupport.class, "map");
+        PROPERTYCHANGESUPPORT_MAP_FIELD_OFFSET = Reflections.getUnsafe().objectFieldOffset(mapField);
+        final Field mapMapField = Reflections.findField(mapField.getType(), "map");
+        PROPERTYCHANGELISTENERMAP_MAP_FIELD_OFFSET = Reflections.getUnsafe().objectFieldOffset(mapMapField);
+    }
+
+    /**
+     * Used to track property change listeners. Lazily initialized. PropertyChangeSupport is itself threadsafe, so we
+     * only need to synchronize when accessing this field. Once the caller has a local reference it can safely access
+     * any of its methods.
+     */
+    @GuardedBy("propertyChangeSupportLock")
+    @Transient
+    @JsonIgnore
+    private transient PropertyChangeSupport propertyChangeSupport;
+    @Transient
+    @JsonIgnore
+    private final Object propertyChangeSupportLock = new Object();
+
+    static {
+        Objects.REFLECTION_EXCLUDED_FIELDS.add("propertyChangeSupportLock");
+        Objects.REFLECTION_EXCLUDED_FIELDS.add("propertyChangeSupport");
+        //need to remove listeners from innerMergeFrom
+        Objects.REFLECTION_EXCLUDED_FIELDS.add("propertyChangeListeners");
+
+    }
+
+    /**
+     * Returns the <code>propertyChangeSupport</code> for this instance, lazily initialized if necessary.
+     * 
+     * @param initializeIfNull
+     *            if <code>true</code>, the instance field is initialized if it's <code>null</code>. If fase, the
+     *            <code>null</code> is returned.
+     * @return the <code>propertyChangeSupport</code> for this instance, which may be <code>null</code> if the
+     *         <code>initializeIfNull</code> parameter is <code>false</code>.
+     */
+    private PropertyChangeSupport lazyGetPropertyChangeSupport(final boolean initializeIfNull) {
+        synchronized (propertyChangeSupportLock) {
+            if (initializeIfNull && propertyChangeSupport == null) {
+                propertyChangeSupport = new PropertyChangeSupport(this);
+            }
+            return propertyChangeSupport;
+        }
+    }
+
+    /**
+     * @see PropertyChangeSupported#addPropertyChangeListener(java.beans.PropertyChangeListener)
+     */
+    @Hidden(skip = true)
+    public final void addPropertyChangeListener(final PropertyChangeListener listener) {
+        if (listener != null) {
+            lazyGetPropertyChangeSupport(true).addPropertyChangeListener(listener);
+        }
+    }
+
+    /**
+     * @see PropertyChangeSupported#addPropertyChangeListener(java.lang.String, java.beans.PropertyChangeListener)
+     */
+    @Hidden(skip = true)
+    public final void addPropertyChangeListener(final String property, final PropertyChangeListener listener) {
+        if (listener != null) {
+            lazyGetPropertyChangeSupport(true).addPropertyChangeListener(property, listener);
+        }
+    }
+
+    /**
+     * @see PropertyChangeSupported#removePropertyChangeListener(java.beans.PropertyChangeListener)
+     */
+    @Hidden(skip = true)
+    public final void removePropertyChangeListener(final PropertyChangeListener listener) {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(false);
+        if (ref != null) {
+            ref.removePropertyChangeListener(listener);
+        }
+    }
+
+    /**
+     * @see PropertyChangeSupported#removePropertyChangeListener(java.lang.String, java.beans.PropertyChangeListener)
+     */
+    @Hidden(skip = true)
+    public final void removePropertyChangeListener(final String property, final PropertyChangeListener listener) {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(false);
+        if (ref != null) {
+            ref.removePropertyChangeListener(property, listener);
+        }
+    }
+
+    /**
+     * @see PropertyChangeSupported#hasListeners(java.lang.String)
+     */
+    @Hidden(skip = true)
+    public final boolean hasListeners(final String propertyName) {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(false);
+        return ref != null && ref.hasListeners(propertyName);
+    }
+
+    @Hidden(skip = true)
+    public boolean hasListeners() {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(false);
+        return ref != null && ref.getPropertyChangeListeners().length > 0;
+    }
+
+    /**
+     * @see PropertyChangeSupported#getPropertyChangeListeners()
+     */
+    @Transient
+    @Hidden(skip = true)
+    @JsonIgnore
+    public final PropertyChangeListener[] getPropertyChangeListeners() {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(true);
+        return ref.getPropertyChangeListeners();
+    }
+
+    /**
+     * @see PropertyChangeSupported#getPropertyChangeListeners(java.lang.String)
+     */
+    @Transient
+    @Hidden(skip = true)
+    @JsonIgnore
+    public final PropertyChangeListener[] getPropertyChangeListeners(final String propertyName) {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(true);
+        return ref.getPropertyChangeListeners(propertyName);
+    }
+
+    /**
+     * This is the same as in the original java beans implementation with the exception that changes from null to null
+     * are not reported as property change events!
+     * 
+     * @see java.beans.PropertyChangeSupport#fireIndexedPropertyChange(java.lang.String, int, java.lang.Object,
+     *      java.lang.Object)
+     */
+    public void fireIndexedPropertyChange(final String propertyName, final int index, final Object oldValue,
+            final Object newValue) {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(false);
+        if (ref != null && (!Objects.equalsProperty(oldValue, newValue)
+                || !equalsPropertyChangeListeners(oldValue, newValue))) {
+            final IndexedPropertyChangeEvent event = new IndexedPropertyChangeEvent(this, propertyName, oldValue,
+                    newValue, index);
+            fireEvent(ref, propertyName, event);
+        }
+    }
+
+    /**
+     * This is the same as in the original java beans implementation with the exception that changes from null to null
+     * are not reported as property change events!
+     * 
+     * @see java.beans.PropertyChangeSupport#firePropertyChange(java.lang.String, java.lang.Object, java.lang.Object)
+     */
+    public void firePropertyChange(final String propertyName, final Object oldValue, final Object newValue) {
+        final PropertyChangeSupport ref = lazyGetPropertyChangeSupport(false);
+        if (ref != null && (!Objects.equalsProperty(oldValue, newValue)
+                || !equalsPropertyChangeListeners(oldValue, newValue))) {
+            final PropertyChangeEvent event = new PropertyChangeEvent(this, propertyName, oldValue, newValue);
+            fireEvent(ref, propertyName, event);
+        }
+    }
+
+    /**
+     * Performance optimization to just fire the events instead of having PropertyChangeSupport call equals so often.
+     * Also with this we don't have to rely on equals alone to fire an event.
+     */
+    @SuppressWarnings("unchecked")
+    private static void fireEvent(final PropertyChangeSupport ref, final String propertyName,
+            final PropertyChangeEvent event) {
+        final Object map = Reflections.getUnsafe().getObject(ref, PROPERTYCHANGESUPPORT_MAP_FIELD_OFFSET);
+        final Map<String, PropertyChangeListener[]> mapMap = (Map<String, PropertyChangeListener[]>) Reflections
+                .getUnsafe()
+                .getObject(map, PROPERTYCHANGELISTENERMAP_MAP_FIELD_OFFSET);
+        if (mapMap == null) {
+            return;
+        }
+        final PropertyChangeListener[] common = mapMap.get(null);
+        fireEvent(common, event);
+        if (propertyName != null) {
+            final PropertyChangeListener[] named = mapMap.get(propertyName);
+            fireEvent(named, event);
+        }
+    }
+
+    private static void fireEvent(final PropertyChangeListener[] listeners, final PropertyChangeEvent event) {
+        if (listeners != null) {
+            for (final PropertyChangeListener listener : listeners) {
+                listener.propertyChange(event);
+            }
+        }
+    }
+
+    private static boolean equalsPropertyChangeListeners(final Object oldValue, final Object newValue) {
+        final APropertyChangeSupportedBase cOldValue;
+        if (oldValue instanceof APropertyChangeSupportedBase) {
+            cOldValue = (APropertyChangeSupportedBase) oldValue;
+        } else {
+            cOldValue = null;
+        }
+        final APropertyChangeSupportedBase cNewValue;
+        if (newValue instanceof APropertyChangeSupportedBase) {
+            cNewValue = (APropertyChangeSupportedBase) newValue;
+        } else {
+            cNewValue = null;
+        }
+        //one of them null
+        if ((cOldValue == null) != (cNewValue == null)) {
+            return false;
+        }
+        //both null
+        if (cOldValue == null) {
+            return true;
+        }
+        final PropertyChangeSupport oldValueRef = cOldValue.lazyGetPropertyChangeSupport(false);
+        final PropertyChangeSupport newValueRef = cNewValue.lazyGetPropertyChangeSupport(false);
+        //one of them null
+        if ((oldValueRef == null) != (newValueRef == null)) {
+            return false;
+        }
+        //both null
+        if (oldValueRef == null) {
+            return true;
+        }
+        return Objects.equals(oldValueRef.getPropertyChangeListeners(), newValueRef.getPropertyChangeListeners());
+    }
+
+}
---
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/swing/text/KeyGrabberTextField.java
@@ -0,0 +1,132 @@
+package de.invesdwin.util.swing.text;
+
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+@NotThreadSafe
+public class KeyGrabberTextField extends JTextField implements FocusListener, KeyListener {
+
+    public static final String ENTER_HOTKEY_TEXT = "Enter hotkey";
+    public static final String DISABLED_TEXT = "Disabled";
+    public static final String PROP_KEY = "key";
+    public static final String PROP_MODIFIERS = "modifiers";
+    private int key = 0;
+    private int modifiers = 0;
+    private boolean singleKeyEnabled = false;
+    private boolean editing = false;
+
+    public KeyGrabberTextField() {
+        super();
+    }
+
+    public KeyGrabberTextField(final boolean enableSingleKey) {
+        super();
+        addFocusListener(this);
+        addKeyListener(this);
+        singleKeyEnabled = enableSingleKey;
+    }
+
+    private void printText() {
+        if (editing) {
+            setText(ENTER_HOTKEY_TEXT);
+        } else {
+            if (key != KeyEvent.VK_UNDEFINED) {
+                String text = KeyEvent.getKeyModifiersText(modifiers);
+                if (text.length() > 0) {
+                    text += "+";
+                }
+                text += KeyEvent.getKeyText(key);
+                setText(text);
+            } else {
+                setText(DISABLED_TEXT);
+            }
+        }
+    }
+
+    @Override
+    public void focusGained(final FocusEvent e) {
+        updateOnFocus(true);
+    }
+
+    @Override
+    public void focusLost(final FocusEvent e) {
+        updateOnFocus(false);
+    }
+
+    @Override
+    public void keyTyped(final KeyEvent e) {
+        e.consume();
+    }
+
+    @Override
+    public void keyPressed(final KeyEvent e) {
+        if (e.getModifiers() == 0 && e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+            setKey(KeyEvent.VK_UNDEFINED);
+            setModifiers(0);
+            updateOnFocus(false);
+        } else {
+            if (isKeyCodeNoModifier(e) && (e.getModifiers() != 0 || singleKeyEnabled)) {
+                setKey(e.getKeyCode());
+                setModifiers(e.getModifiers());
+                updateOnFocus(false);
+            }
+        }
+        e.consume();
+    }
+
+    private void updateOnFocus(final boolean focus) {
+        editing = focus;
+        if (focus) {
+            setBackground((Color) UIManager.get("List.selectionBackground"));
+            setForeground((Color) UIManager.get("List.selectionForeground"));
+            getCaret().setSelectionVisible(false);
+            getCaret().setVisible(false);
+            printText();
+        } else {
+            setBackground((Color) UIManager.get("TextArea.background"));
+            setForeground((Color) UIManager.get("TextArea.foreground"));
+            printText();
+        }
+        repaint();
+    }
+
+    private boolean isKeyCodeNoModifier(final KeyEvent e) {
+        return e.getKeyCode() != KeyEvent.VK_SHIFT && e.getKeyCode() != KeyEvent.VK_CONTROL
+                && e.getKeyCode() != KeyEvent.VK_META && e.getKeyCode() != KeyEvent.VK_ALT
+                && e.getKeyCode() != KeyEvent.VK_ALT_GRAPH;
+    }
+
+    @Override
+    public void keyReleased(final KeyEvent e) {
+        e.consume();
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int getModifiers() {
+        return modifiers;
+    }
+
+    public void setKey(final int key) {
+        final int oldKey = this.key;
+        this.key = key;
+        firePropertyChange(PROP_KEY, oldKey, key);
+        printText();
+    }
+
+    public void setModifiers(final int modifiers) {
+        final int oldModifiers = this.modifiers;
+        this.modifiers = modifiers;
+        firePropertyChange(PROP_MODIFIERS, oldModifiers, modifiers);
+        printText();
+    }
+}
