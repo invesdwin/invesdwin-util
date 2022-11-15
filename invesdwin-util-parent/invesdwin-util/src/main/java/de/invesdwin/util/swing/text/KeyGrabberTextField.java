c7f7461add7c389ba52d585d9404bcaa81906942
Edwin Stang <gsubes@gmail.com>
2022-11-15 12:54:55 +0100

    PER-223 support disabling the text field, make ESC cancel and CTRL+ESC clear, also support middle mouse to clear the hotkey; allow exiting edit mode by clicking a second time; work nicely with text selection to copy the hotkey to clipboard

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/swing/text/KeyGrabberTextField.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/swing/text/KeyGrabberTextField.java
@@ -1,6 +1,5 @@
 package de.invesdwin.util.swing.text;
 
-import java.awt.Color;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
@@ -13,32 +12,40 @@ import javax.annotation.concurrent.NotThreadSafe;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 
+import de.invesdwin.util.lang.string.Strings;
+import de.invesdwin.util.swing.Components;
+
 @NotThreadSafe
 public class KeyGrabberTextField extends JTextField implements FocusListener, KeyListener, MouseListener {
 
-    public static final String ENTER_HOTKEY_TEXT = "Enter hotkey";
-    public static final String DISABLED_TEXT = "Disabled";
+    public static final boolean DEFAULT_SINGLE_KEY_ENABLED = true;
     public static final String PROP_HOTKEY = "hotkey";
 
-    private int key = 0;
-    private int modifiers = 0;
-    private boolean singleKeyEnabled = false;
-    private boolean editing = false;
+    private static final String ENTER_HOTKEY_TEXT = "Enter hotkey";
+    private static final String DISABLED_TEXT = "Disabled";
+
+    private final boolean singleKeyEnabled;
+    private int key;
+    private int modifiers;
+    private boolean editing;
 
     public KeyGrabberTextField() {
-        super();
+        this(DEFAULT_SINGLE_KEY_ENABLED);
     }
 
-    public KeyGrabberTextField(final boolean enableSingleKey) {
-        super();
+    public KeyGrabberTextField(final boolean singleKeyEnabled) {
         addFocusListener(this);
         addKeyListener(this);
         addMouseListener(this);
         super.setEditable(false);
-        updateOnFocus(false);
+        updateOnEditing(false);
         setFocusable(false);
 
-        singleKeyEnabled = enableSingleKey;
+        //don't show caret
+        getCaret().setSelectionVisible(false);
+        getCaret().setVisible(false);
+
+        this.singleKeyEnabled = singleKeyEnabled;
     }
 
     private void printText() {
@@ -46,7 +53,7 @@ public class KeyGrabberTextField extends JTextField implements FocusListener, Ke
             super.setText(ENTER_HOTKEY_TEXT);
         } else {
             if (key != KeyEvent.VK_UNDEFINED) {
-                String text = KeyEvent.getKeyModifiersText(modifiers);
+                String text = KeyEvent.getModifiersExText(modifiers);
                 if (text.length() > 0) {
                     text += "+";
                 }
@@ -60,46 +67,57 @@ public class KeyGrabberTextField extends JTextField implements FocusListener, Ke
 
     @Override
     public void focusLost(final FocusEvent e) {
-        updateOnFocus(false);
+        if (editing) {
+            //keep caret where it is when popup menu is shown
+            updateOnEditing(false);
+        }
         setFocusable(false);
     }
 
     @Override
     public void keyTyped(final KeyEvent e) {
-        e.consume();
+        if (editing) {
+            e.consume();
+        }
     }
 
     @Override
     public void keyPressed(final KeyEvent e) {
-        if (!editing) {
+        if (editing) {
             e.consume();
+        }
+        if (!editing || !isEnabled()) {
             return;
         }
-
-        if (e.getModifiers() == KeyEvent.VK_UNDEFINED && e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+        if (e.getModifiersEx() == KeyEvent.VK_UNDEFINED && e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            //escape cancels and keeps old hotkey
+            updateOnEditing(false);
+        } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            //ctrl+escape disables the hotkey
             setHotkey(KeyEvent.VK_UNDEFINED, KeyEvent.VK_UNDEFINED);
-            updateOnFocus(false);
-        } else {
-            if (isKeyCodeNoModifier(e) && (e.getModifiers() != KeyEvent.VK_UNDEFINED || singleKeyEnabled)) {
-                setHotkey(e.getKeyCode(), e.getModifiers());
-                updateOnFocus(false);
-            }
+            updateOnEditing(false);
+        } else if (isKeyCodeNoModifier(e) && (e.getModifiersEx() != KeyEvent.VK_UNDEFINED || singleKeyEnabled)) {
+            setHotkey(e.getKeyCode(), e.getModifiersEx());
+            updateOnEditing(false);
         }
-
-        e.consume();
     }
 
-    private void updateOnFocus(final boolean focus) {
-        editing = focus;
-        if (focus) {
-            setBackground((Color) UIManager.get("List.selectionBackground"));
-            setForeground((Color) UIManager.get("List.selectionForeground"));
-            getCaret().setSelectionVisible(false);
-            getCaret().setVisible(false);
+    private void updateOnEditing(final boolean newEditing) {
+        if (!isEnabled()) {
+            Components.setBackground(this, UIManager.getColor("TextField.inactiveBackground"));
+            Components.setForeground(this, UIManager.getColor("TextField.inactiveForeground"));
+            editing = false;
+            printText();
+            return;
+        }
+        editing = newEditing;
+        if (newEditing) {
+            Components.setBackground(this, UIManager.getColor("TextField.selectionBackground"));
+            Components.setForeground(this, UIManager.getColor("TextField.selectionBackground"));
             printText();
         } else {
-            setBackground((Color) UIManager.get("TextArea.background"));
-            setForeground((Color) UIManager.get("TextArea.foreground"));
+            Components.setBackground(this, UIManager.getColor("TextField.background"));
+            Components.setForeground(this, UIManager.getColor("TextField.foreground"));
             printText();
         }
     }
@@ -112,7 +130,9 @@ public class KeyGrabberTextField extends JTextField implements FocusListener, Ke
 
     @Override
     public void keyReleased(final KeyEvent e) {
-        e.consume();
+        if (editing) {
+            e.consume();
+        }
     }
 
     @Override
@@ -145,25 +165,60 @@ public class KeyGrabberTextField extends JTextField implements FocusListener, Ke
     }
 
     @Override
-    public void mouseReleased(final MouseEvent e) {
+    public void mousePressed(final MouseEvent e) {
+        if (editing) {
+            e.consume();
+        }
         setFocusable(true);
         requestFocus();
-        updateOnFocus(true);
     }
 
     @Override
-    public boolean isEditable() {
-        return false;
+    public void mouseReleased(final MouseEvent e) {
+        if (editing) {
+            e.consume();
+        }
+    }
+
+    @Override
+    public void setEnabled(final boolean enabled) {
+        final boolean oldEnabled = isEnabled();
+        super.setEnabled(enabled);
+        if (oldEnabled != enabled) {
+            updateOnEditing(editing);
+        }
     }
 
     @Override
     public void mouseClicked(final MouseEvent e) {
-        //noop
-    }
-
-    @Override
-    public void mousePressed(final MouseEvent e) {
-        //noop
+        if (!isEnabled()) {
+            return;
+        }
+        if (editing) {
+            e.consume();
+            if (e.getButton() == MouseEvent.BUTTON2 && e.getClickCount() == 1) {
+                //middle mouse removes the hotkey
+                setHotkey(KeyEvent.VK_UNDEFINED, KeyEvent.VK_UNDEFINED);
+                updateOnEditing(false);
+            } else if (e.getButton() == MouseEvent.BUTTON1) {
+                //disable editing when clicked again
+                updateOnEditing(false);
+            }
+        } else {
+            if (!contains(e.getX(), e.getY())) {
+                return;
+            }
+            if (Strings.isNotBlank(getSelectedText())) {
+                return;
+            }
+            if (e.getClickCount() != 1) {
+                return;
+            }
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                //start editing when clicked into
+                updateOnEditing(true);
+            }
+        }
     }
 
     @Override
@@ -185,4 +240,18 @@ public class KeyGrabberTextField extends JTextField implements FocusListener, Ke
     public void setEditable(final boolean b) {
         //noop
     }
+
+    @Override
+    public boolean isEditable() {
+        return false;
+    }
+
+    @Override
+    public void setToolTipText(final String text) {
+        if (editing) {
+            return;
+        }
+        super.setToolTipText(text);
+    }
+
 }
