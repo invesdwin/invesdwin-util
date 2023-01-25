447e2475f22976945fe2b9fe5abbc015ad4b0aae
Edwin Stang <gsubes@gmail.com>
2023-01-25 13:34:45 +0100

    PER-25 go back to skipping null elements as before

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/loadingcache/historical/query/internal/core/AFilteringDelegateHistoricalCacheQueryCore.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/loadingcache/historical/query/internal/core/AFilteringDelegateHistoricalCacheQueryCore.java
@@ -2,7 +2,6 @@ package de.invesdwin.util.collections.loadingcache.historical.query.internal.cor
 
 import javax.annotation.concurrent.Immutable;
 
-import de.invesdwin.util.collections.iterable.ATransformingIterable;
 import de.invesdwin.util.collections.iterable.ICloseableIterable;
 import de.invesdwin.util.collections.iterable.skip.ASkippingIterable;
 import de.invesdwin.util.collections.loadingcache.historical.IHistoricalEntry;
@@ -42,28 +41,16 @@ public abstract class AFilteringDelegateHistoricalCacheQueryCore<V> implements I
     }
 
     private ICloseableIterable<IHistoricalEntry<V>> filter(final ICloseableIterable<IHistoricalEntry<V>> entries) {
-        final ASkippingIterable<IHistoricalEntry<V>> skipping = new ASkippingIterable<IHistoricalEntry<V>>(entries) {
+        return new ASkippingIterable<IHistoricalEntry<V>>(entries) {
             @Override
             protected boolean skip(final IHistoricalEntry<V> element) {
                 if (element == null) {
                     return true;
                 }
-                return false;
-            }
-        };
-        return new ATransformingIterable<IHistoricalEntry<V>, IHistoricalEntry<V>>(skipping) {
-
-            @Override
-            protected IHistoricalEntry<V> transform(final IHistoricalEntry<V> entry) {
-                //check if present to prevent stack overflow during lazy loading
-                if (entry.isValuePresent() && entry.getValueIfPresent() == null) {
-                    if (entry.getKey() != null) {
-                        return ImmutableHistoricalEntry.of(entry.getKey(), null);
-                    } else {
-                        return null;
-                    }
+                if (element.isValuePresent() && element.getValueIfPresent() == null) {
+                    return true;
                 }
-                return entry;
+                return false;
             }
         };
     }
