917c660719107cebdf030daacaab6430806d30f5
Edwin Stang <gsubes@gmail.com>
2023-03-15 12:23:45 +0100

    PER-7 locks are now closeable

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/lock/ILock.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/lock/ILock.java
@@ -1,9 +1,15 @@
 package de.invesdwin.util.concurrent.lock;
 
+import java.io.Closeable;
 import java.util.concurrent.locks.Lock;
 
-public interface ILock extends Lock {
+public interface ILock extends Lock, Closeable {
 
     String getName();
+
+    @Override
+    default void close() {
+        unlock();
+    }
 
 }
