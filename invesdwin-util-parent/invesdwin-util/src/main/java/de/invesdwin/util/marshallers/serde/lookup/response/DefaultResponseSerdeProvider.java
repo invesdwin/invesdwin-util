8decda35816acb0e324f0d2920235f41151d15c3
Edwin Stang <gsubes@gmail.com>
2023-06-13 21:32:35 +0200

    instantiate serde for future type param

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/marshallers/serde/lookup/response/DefaultResponseSerdeProvider.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/marshallers/serde/lookup/response/DefaultResponseSerdeProvider.java
@@ -1,9 +1,13 @@
 package de.invesdwin.util.marshallers.serde.lookup.response;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.concurrent.Future;
 
 import javax.annotation.concurrent.Immutable;
 
+import de.invesdwin.util.lang.reflection.Reflections;
 import de.invesdwin.util.marshallers.serde.ISerde;
 import de.invesdwin.util.marshallers.serde.TypeDelegateSerde;
 
@@ -14,7 +18,18 @@ public final class DefaultResponseSerdeProvider implements IResponseSerdeProvide
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public DefaultResponseSerdeProvider(final Method method) {
-        this.serde = new TypeDelegateSerde(method.getReturnType());
+        Class<?> returnType = method.getReturnType();
+        if (Future.class.isAssignableFrom(returnType)) {
+            final Type genericReturnType = method.getGenericReturnType();
+            if (genericReturnType instanceof ParameterizedType) {
+                final ParameterizedType parameterizedType = (ParameterizedType) genericReturnType;
+                final Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+                if (actualTypeArguments.length == 1) {
+                    returnType = Reflections.determineClassType(actualTypeArguments[0]);
+                }
+            }
+        }
+        this.serde = new TypeDelegateSerde(returnType);
     }
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
