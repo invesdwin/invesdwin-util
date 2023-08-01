b706c0333b35b4ba906ff0de7ec9622a6ebef1fc
subes <subes@subes-lappy>
2023-08-01 14:12:18 +0200

    PER-326 make effectBarId comparisons faster and less wasteful

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/time/date/FDates.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/time/date/FDates.java
@@ -654,6 +654,17 @@ public final class FDates {
         return a.compareToNotNullSafe(b);
     }
 
+    public static int compareInvertNull(final FDate a, final FDate b) {
+        if (a == null && b == null) {
+            return 0;
+        } else if (a == null) {
+            return 1;
+        } else if (b == null) {
+            return -1;
+        }
+        return a.compareToNotNullSafe(b);
+    }
+
     public static int compareNotNullSafe(final FDate a, final FDate b) {
         return a.compareToNotNullSafe(b);
     }
