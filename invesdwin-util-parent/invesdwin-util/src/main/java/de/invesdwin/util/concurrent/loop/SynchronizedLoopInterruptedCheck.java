ddf43ce191964e4bd79e165af49b85eb6516afe2
Edwin Stang <gsubes@gmail.com>
2023-08-19 12:17:10 +0200

    use tryLock instead of synchronized to further reduce lock contention

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/loop/SynchronizedLoopInterruptedCheck.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/loop/SynchronizedLoopInterruptedCheck.java
@@ -4,7 +4,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.concurrent.ThreadSafe;
 
+import de.invesdwin.util.collections.factory.ILockCollectionFactory;
 import de.invesdwin.util.concurrent.Threads;
+import de.invesdwin.util.concurrent.lock.ILock;
 import de.invesdwin.util.time.date.FTimeUnit;
 import de.invesdwin.util.time.duration.Duration;
 
@@ -16,6 +18,8 @@ public class SynchronizedLoopInterruptedCheck {
     private volatile long nextIntervalNanos;
     private volatile int checksPerInterval;
     private final AtomicInteger checksInInterval = new AtomicInteger();
+    private final ILock lock = ILockCollectionFactory.getInstance(true)
+            .newLock(SynchronizedLoopInterruptedCheck.class.getSimpleName());
 
     public SynchronizedLoopInterruptedCheck() {
         this(Duration.ONE_SECOND);
@@ -40,13 +44,17 @@ public class SynchronizedLoopInterruptedCheck {
         if (checksInIntervalValue > checksPerInterval) {
             final long newIntervalNanos = getInitialNanoTime();
             if (newIntervalNanos > nextIntervalNanos) {
-                synchronized (this) {
-                    if (newIntervalNanos > nextIntervalNanos) {
-                        final boolean result = onInterval();
-                        checksPerInterval = checksInIntervalValue;
-                        checksInInterval.set(0);
-                        nextIntervalNanos = newIntervalNanos + checkIntervalNanos;
-                        return result;
+                if (lock.tryLock()) {
+                    try {
+                        if (newIntervalNanos > nextIntervalNanos) {
+                            final boolean result = onInterval();
+                            checksPerInterval = checksInIntervalValue;
+                            checksInInterval.set(0);
+                            nextIntervalNanos = newIntervalNanos + checkIntervalNanos;
+                            return result;
+                        }
+                    } finally {
+                        lock.unlock();
                     }
                 }
             }
