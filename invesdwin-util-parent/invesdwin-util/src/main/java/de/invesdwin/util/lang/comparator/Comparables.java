17c48cca05dea2f81e3727cf3d33ae334680ccc4
Edwin Stang <gsubes@gmail.com>
2023-09-05 13:41:45 +0200

    PER-448 support for navigable persistent map range table

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/comparator/Comparables.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/lang/comparator/Comparables.java
@@ -1,11 +1,41 @@
 package de.invesdwin.util.lang.comparator;
 
+import java.util.Comparator;
+
 import javax.annotation.concurrent.Immutable;
 
 @Immutable
 public final class Comparables {
 
+    @SuppressWarnings("rawtypes")
+    private static final Comparator COMPARATOR = new Comparator() {
+
+        @Override
+        public int compare(final Object o1, final Object o2) {
+            return Comparables.compare((Comparable) o1, (Comparable) o2);
+        }
+    };
+
+    @SuppressWarnings("rawtypes")
+    private static final Comparator COMPARATOR_NOT_NULL_SAFE = new Comparator() {
+
+        @Override
+        public int compare(final Object o1, final Object o2) {
+            return Comparables.compareNotNullSafe((Comparable) o1, (Comparable) o2);
+        }
+    };
+
     private Comparables() {}
+
+    @SuppressWarnings("unchecked")
+    public static <T> Comparator<T> getComparator() {
+        return COMPARATOR;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Comparator<T> getComparatorNotNullSafe() {
+        return COMPARATOR_NOT_NULL_SAFE;
+    }
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public static <T extends Comparable> int compare(final T a, final T b) {
---
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/marshallers/serde/NioByteBufferSerde.java
@@ -0,0 +1,38 @@
+package de.invesdwin.util.marshallers.serde;
+
+import javax.annotation.concurrent.Immutable;
+
+import de.invesdwin.util.streams.buffer.bytes.IByteBuffer;
+import de.invesdwin.util.streams.buffer.bytes.extend.UnsafeByteBuffer;
+
+@Immutable
+public class NioByteBufferSerde implements ISerde<java.nio.ByteBuffer> {
+
+    public static final NioByteBufferSerde GET = new NioByteBufferSerde();
+
+    @Override
+    public java.nio.ByteBuffer fromBytes(final byte[] bytes) {
+        return SerdeBaseMethods.fromBytes(this, bytes);
+    }
+
+    @Override
+    public byte[] toBytes(final java.nio.ByteBuffer obj) {
+        return new UnsafeByteBuffer(obj).asByteArray();
+    }
+
+    @Override
+    public java.nio.ByteBuffer fromBuffer(final IByteBuffer buffer) {
+        return buffer.asNioByteBuffer();
+    }
+
+    @Override
+    public int toBuffer(final IByteBuffer buffer, final java.nio.ByteBuffer obj) {
+        final int length = obj.capacity();
+        if (buffer == obj) {
+            return length;
+        } else {
+            buffer.putBytesTo(0, obj, length);
+            return length;
+        }
+    }
+}
