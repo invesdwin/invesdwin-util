d5c3d4fac0f44d8875d64d890806d70c25af780b
Edwin Stang <gsubes@gmail.com>
2023-10-05 23:08:29 +0200

    cmaes and bobyqa optimizers from commons-math3

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/random/RandomGeneratorAdapter.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/random/RandomGeneratorAdapter.java
@@ -5,15 +5,26 @@ import javax.annotation.concurrent.Immutable;
 @Immutable
 public class RandomGeneratorAdapter implements IRandomGenerator {
 
-    private final java.util.Random delegate;
+    private final java.util.random.RandomGenerator randomGenerator;
+    private final java.util.Random random;
 
     public RandomGeneratorAdapter(final java.util.Random delegate) {
-        this.delegate = delegate;
+        this.randomGenerator = delegate;
+        this.random = delegate;
+    }
+
+    public RandomGeneratorAdapter(final java.util.random.RandomGenerator delegate) {
+        this.randomGenerator = delegate;
+        if (delegate instanceof java.util.Random) {
+            this.random = (java.util.Random) delegate;
+        } else {
+            this.random = null;
+        }
     }
 
     @Override
     public String toString() {
-        return delegate.toString();
+        return randomGenerator.toString();
     }
 
     @Override
@@ -35,47 +46,49 @@ public class RandomGeneratorAdapter implements IRandomGenerator {
 
     @Override
     public void setSeed(final long seed) {
-        delegate.setSeed(seed);
+        if (random != null) {
+            random.setSeed(seed);
+        }
     }
 
     @Override
     public void nextBytes(final byte[] bytes) {
-        delegate.nextBytes(bytes);
+        randomGenerator.nextBytes(bytes);
     }
 
     @Override
     public int nextInt() {
-        return delegate.nextInt();
+        return randomGenerator.nextInt();
     }
 
     @Override
     public int nextInt(final int n) {
-        return delegate.nextInt(n);
+        return randomGenerator.nextInt(n);
     }
 
     @Override
     public long nextLong() {
-        return delegate.nextLong();
+        return randomGenerator.nextLong();
     }
 
     @Override
     public boolean nextBoolean() {
-        return delegate.nextBoolean();
+        return randomGenerator.nextBoolean();
     }
 
     @Override
     public float nextFloat() {
-        return delegate.nextFloat();
+        return randomGenerator.nextFloat();
     }
 
     @Override
     public double nextDouble() {
-        return delegate.nextDouble();
+        return randomGenerator.nextDouble();
     }
 
     @Override
     public double nextGaussian() {
-        return delegate.nextGaussian();
+        return randomGenerator.nextGaussian();
     }
 
 }
