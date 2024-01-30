05c35f1249dcc406c5efda2e943b1ef2db559ae0
Edwin Stang <gsubes@gmail.com>
2024-01-30 15:37:26 +0100

    PER-532 don't log unclosed warning for disabled executor

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/Executors.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/Executors.java
@@ -122,6 +122,7 @@ public final class Executors {
 
             {
                 super.setDynamicThreadName(false);
+                super.setFinalizerEnabled(false);
             }
 
             @Override
