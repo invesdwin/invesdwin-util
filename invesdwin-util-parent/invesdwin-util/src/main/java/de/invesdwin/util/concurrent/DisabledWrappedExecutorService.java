852f5ea1b421694766aafb82562464b63c6b0f6e
Edwin Stang <gsubes@gmail.com>
2025-08-27 18:57:56 +0200

    PER-960 add failFast option to DisabledWrappedExecutorService to directly rethrow exceptions instead of waiting for client code to get the exceptions from the futures (useful to fail fast in unit test suites where parallelisation has been disabled to run everything sequentially anyhow)

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/DisabledWrappedExecutorService.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/DisabledWrappedExecutorService.java
@@ -1,20 +1,41 @@
 package de.invesdwin.util.concurrent;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.annotation.concurrent.Immutable;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
 import de.invesdwin.util.collections.Collections;
+import de.invesdwin.util.concurrent.future.Futures;
 import de.invesdwin.util.shutdown.IShutdownHook;
 
 @Immutable
 public class DisabledWrappedExecutorService extends WrappedExecutorService {
+
+    private volatile boolean failFast = false;
+
     public DisabledWrappedExecutorService(final ExecutorService delegate, final String name) {
         super(delegate, name);
         super.setDynamicThreadName(false);
         super.setFinalizerEnabled(false);
+    }
+
+    public DisabledWrappedExecutorService setFailFast(final boolean failFast) {
+        this.failFast = failFast;
+        return this;
+    }
+
+    public boolean isFailFast() {
+        return failFast;
     }
 
     @Override
@@ -67,5 +88,93 @@ public class DisabledWrappedExecutorService extends WrappedExecutorService {
     public WrappedExecutorService setDynamicThreadName(final boolean dynamicThreadName) {
         //disabled
         return this;
+    }
+
+    @Override
+    public <T> T invokeAny(final Collection<? extends Callable<T>> tasks)
+            throws InterruptedException, ExecutionException {
+        //failFast not needed here, since it will anyhow ignore exceptions
+        return super.invokeAny(tasks);
+    }
+
+    @Override
+    public <T> T invokeAny(final Collection<? extends Callable<T>> tasks, final long timeout, final TimeUnit unit)
+            throws InterruptedException, ExecutionException, TimeoutException {
+        //failFast not needed here, since it will anyhow ignore exceptions
+        return super.invokeAny(tasks, timeout, unit);
+    }
+
+    @Override
+    public void execute(final Runnable command) {
+        if (failFast) {
+            final ListenableFuture<?> future = super.submit(command);
+            Futures.waitPropagatingNoInterrupt(future);
+        } else {
+            super.execute(command);
+        }
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submit(final Callable<T> task) {
+        if (failFast) {
+            final ListenableFuture<T> future = super.submit(task);
+            Futures.waitPropagatingNoInterrupt(future);
+            return future;
+        } else {
+            return super.submit(task);
+        }
+    }
+
+    @Override
+    public ListenableFuture<?> submit(final Runnable task) {
+        if (failFast) {
+            final ListenableFuture<?> future = super.submit(task);
+            Futures.waitPropagatingNoInterrupt(future);
+            return future;
+        } else {
+            return super.submit(task);
+        }
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submit(final Runnable task, final T result) {
+        if (failFast) {
+            final ListenableFuture<T> future = super.submit(task, result);
+            Futures.waitPropagatingNoInterrupt(future);
+            return future;
+        } else {
+            return super.submit(task, result);
+        }
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(final Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        if (failFast) {
+            final List<Future<T>> futures = new ArrayList<>(tasks.size());
+            for (final Callable<T> task : tasks) {
+                final ListenableFuture<T> future = super.submit(task);
+                Futures.waitPropagatingNoInterrupt(future);
+                futures.add(future);
+            }
+            return futures;
+        } else {
+            return super.invokeAll(tasks);
+        }
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(final Collection<? extends Callable<T>> tasks, final long timeout,
+            final TimeUnit unit) throws InterruptedException {
+        if (failFast) {
+            final List<Future<T>> futures = new ArrayList<>(tasks.size());
+            for (final Callable<T> task : tasks) {
+                final ListenableFuture<T> future = super.submit(task);
+                Futures.waitPropagatingNoInterrupt(future);
+                futures.add(future);
+            }
+            return futures;
+        } else {
+            return super.invokeAll(tasks);
+        }
     }
 }
