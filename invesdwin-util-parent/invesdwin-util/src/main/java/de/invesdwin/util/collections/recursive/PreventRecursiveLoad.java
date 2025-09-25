b969faeaee5c9d32509ca8e9780d4a4e3ad2b919
matztejankow <m.jankow@1-2-3-invest.de>
2025-09-25 15:28:34 +0200

    PER-1017 prevent deadlock on recursive load from another thread

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/recursive/PreventRecursiveLoad.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/recursive/PreventRecursiveLoad.java
@@ -1,57 +1,55 @@
 package de.invesdwin.util.collections.recursive;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import javax.annotation.concurrent.ThreadSafe;
 
-import de.invesdwin.util.concurrent.reference.WeakThreadLocalReference;
+import de.invesdwin.util.collections.factory.ILockCollectionFactory;
 
 @ThreadSafe
 public class PreventRecursiveLoad<K, V> {
 
-    private final WeakThreadLocalReference<Set<K>> alreadyLoadingKeyRef = new WeakThreadLocalReference<Set<K>>() {
-        @Override
-        protected Set<K> initialValue() {
-            return new HashSet<K>();
-        }
-    };
+    private final Set<K> alreadyLoadingKeySet = ILockCollectionFactory.getInstance(true).newConcurrentSet();
 
     public PreventRecursiveLoad() {}
 
+    public void preventRecursiveLoad(final K key) throws RecursiveLoadException {
+        if (alreadyLoadingKeySet.contains(key)) {
+            throw newRecursiveLoadException(key);
+        }
+    }
+
     public V preventRecursiveLoad(final K key, final Function<? super K, ? extends V> delegate)
             throws RecursiveLoadException {
-        final Set<K> alreadyLoading = alreadyLoadingKeyRef.get();
-        if (alreadyLoading.add(key)) {
+        if (alreadyLoadingKeySet.add(key)) {
             try {
                 final V loaded = delegate.apply(key);
                 return loaded;
             } finally {
-                alreadyLoading.remove(key);
+                alreadyLoadingKeySet.remove(key);
             }
         } else {
-            return onRecursiveLoad(key);
+            throw newRecursiveLoadException(key);
         }
     }
 
     public V preventRecursiveLoad(final K key, final Supplier<? extends V> delegate) throws RecursiveLoadException {
-        final Set<K> alreadyLoading = alreadyLoadingKeyRef.get();
-        if (alreadyLoading.add(key)) {
+        if (alreadyLoadingKeySet.add(key)) {
             try {
                 final V loaded = delegate.get();
                 return loaded;
             } finally {
-                alreadyLoading.remove(key);
+                alreadyLoadingKeySet.remove(key);
             }
         } else {
-            return onRecursiveLoad(key);
+            throw newRecursiveLoadException(key);
         }
     }
 
-    protected V onRecursiveLoad(final K key) throws RecursiveLoadException {
-        throw FastRecursiveLoadException.getInstance("Already loading recursively key: %s", key);
+    protected RecursiveLoadException newRecursiveLoadException(final K key) {
+        return FastRecursiveLoadException.getInstance("Already loading recursively key: %s", key);
     }
 
 }
