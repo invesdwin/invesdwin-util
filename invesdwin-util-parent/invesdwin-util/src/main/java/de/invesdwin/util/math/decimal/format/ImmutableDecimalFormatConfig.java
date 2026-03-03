448a2177d06aee958b60c8739e6247c9be70cfae
Edwin Stang <gsubes@gmail.com>
2026-03-03 15:53:16 +0100

    PER-1190 fix another memory leak

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/format/ImmutableDecimalFormatConfig.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/format/ImmutableDecimalFormatConfig.java
@@ -4,17 +4,25 @@ import java.text.DecimalFormatSymbols;
 
 import javax.annotation.concurrent.Immutable;
 
+import de.invesdwin.util.lang.Objects;
+
 @Immutable
 public final class ImmutableDecimalFormatConfig {
 
     private final String format;
     private final DecimalFormatSymbols symbols;
     private final int multiplier;
+    private final int hashCode;
 
     public ImmutableDecimalFormatConfig(final String format, final DecimalFormatSymbols symbols, final int multiplier) {
         this.format = format;
         this.symbols = symbols;
         this.multiplier = multiplier;
+        this.hashCode = newHashCode();
+    }
+
+    private int newHashCode() {
+        return Objects.hashCode(format, symbols, multiplier);
     }
 
     public String getFormat() {
@@ -27,6 +35,30 @@ public final class ImmutableDecimalFormatConfig {
 
     public int getMultiplier() {
         return multiplier;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj instanceof ImmutableDecimalFormatConfig) {
+            final ImmutableDecimalFormatConfig cObj = (ImmutableDecimalFormatConfig) obj;
+            return Objects.equals(format, cObj.format) && Objects.equals(symbols, cObj.symbols)
+                    && multiplier == cObj.multiplier;
+        }
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return hashCode;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("format", format)
+                .add("symbols", symbols)
+                .add("multiplier", multiplier)
+                .toString();
     }
 
 }
