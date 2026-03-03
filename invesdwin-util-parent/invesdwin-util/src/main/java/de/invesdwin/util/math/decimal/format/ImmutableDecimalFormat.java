0354c4af2c9a0a7c9de54c0850e1bd2efadad2e8
Edwin Stang <gsubes@gmail.com>
2026-03-03 15:33:52 +0100

    PER-1190 init fix

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/format/ImmutableDecimalFormat.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/math/decimal/format/ImmutableDecimalFormat.java
@@ -12,112 +12,183 @@ import de.invesdwin.util.math.decimal.ADecimal;
 @Immutable
 public final class ImmutableDecimalFormat extends DecimalFormat {
 
+    private final boolean initialized;
+
     public ImmutableDecimalFormat(final ImmutableDecimalFormatConfig config) {
         super(config.getFormat(), config.getSymbols());
         super.setRoundingMode(ADecimal.DEFAULT_ROUNDING_MODE);
         super.setMultiplier(config.getMultiplier());
+        initialized = true;
     }
 
     @Deprecated
     @Override
     public void setCurrency(final Currency currency) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setCurrency(currency);
+        }
     }
 
     @Deprecated
     @Override
     public void setDecimalFormatSymbols(final DecimalFormatSymbols newSymbols) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setDecimalFormatSymbols(newSymbols);
+        }
     }
 
     @Deprecated
     @Override
     public void setDecimalSeparatorAlwaysShown(final boolean newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setDecimalSeparatorAlwaysShown(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setGroupingSize(final int newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setGroupingSize(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setGroupingUsed(final boolean newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setGroupingUsed(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setMaximumFractionDigits(final int newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setMaximumFractionDigits(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setMaximumIntegerDigits(final int newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setMaximumIntegerDigits(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setMinimumFractionDigits(final int newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setMinimumFractionDigits(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setMinimumIntegerDigits(final int newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setMinimumIntegerDigits(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setMultiplier(final int newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setMultiplier(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setNegativePrefix(final String newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setNegativePrefix(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setNegativeSuffix(final String newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setNegativeSuffix(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setParseBigDecimal(final boolean newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setParseBigDecimal(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setParseIntegerOnly(final boolean value) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setParseIntegerOnly(value);
+        }
     }
 
     @Deprecated
     @Override
     public void setPositivePrefix(final String newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setPositivePrefix(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setPositiveSuffix(final String newValue) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setPositiveSuffix(newValue);
+        }
     }
 
     @Deprecated
     @Override
     public void setRoundingMode(final RoundingMode roundingMode) {
-        throw newUnsupportedOperationException();
+        if (initialized) {
+            throw newUnsupportedOperationException();
+        } else {
+            super.setRoundingMode(roundingMode);
+        }
     }
 
     private UnsupportedOperationException newUnsupportedOperationException() {
