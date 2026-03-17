0883ce7db513697b39e22337a859a8a9637944f3
Edwin Stang <gsubes@gmail.com>
2026-03-17 23:44:20 +0100

    PER-1216 retry flush

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/DisabledCloseableIterable.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/collections/iterable/DisabledCloseableIterable.java
@@ -3,7 +3,7 @@ package de.invesdwin.util.collections.iterable;
 import javax.annotation.concurrent.Immutable;
 
 @Immutable
-public final class DisabledCloseableIterable<E> implements IPeekingCloseableIterable<E> {
+public final class DisabledCloseableIterable<E> implements IPeekingCloseableIterable<E>, IReverseCloseableIterable<E> {
 
     @SuppressWarnings("rawtypes")
     private static final DisabledCloseableIterable INSTANCE = new DisabledCloseableIterable();
@@ -18,6 +18,11 @@ public final class DisabledCloseableIterable<E> implements IPeekingCloseableIter
     @SuppressWarnings("unchecked")
     public static <T> DisabledCloseableIterable<T> getInstance() {
         return INSTANCE;
+    }
+
+    @Override
+    public ICloseableIterator<E> reverseIterator() {
+        return DisabledCloseableIterator.getInstance();
     }
 
 }
