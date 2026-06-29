4f6a0cd6695799e4e60da411d7156c47e3da0e9c
Edwin Stang <gsubes@gmail.com>
2026-06-29 14:13:23 +0200

    PER-1256 support closing threadlocal references when they are removed

--- invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/reference/WeakThreadLocalReference.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/de/invesdwin/util/concurrent/reference/WeakThreadLocalReference.java
@@ -4,9 +4,14 @@ import java.util.Optional;
 
 import javax.annotation.concurrent.ThreadSafe;
 
+import org.jspecify.annotations.Nullable;
+
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.github.benmanes.caffeine.cache.RemovalCause;
 
+import de.invesdwin.util.streams.closeable.Closeables;
+import de.invesdwin.util.streams.closeable.ISafeCloseable;
 import io.netty.util.concurrent.FastThreadLocal;
 
 /**
@@ -19,19 +24,38 @@ import io.netty.util.concurrent.FastThreadLocal;
 @ThreadSafe
 public class WeakThreadLocalReference<V> implements IMutableReference<V> {
 
-    private static final FastThreadLocal<LoadingCache<Object, IMutableReference<Optional<Object>>>> THREAD_LOCAL = new FastThreadLocal<LoadingCache<Object, IMutableReference<Optional<Object>>>>() {
+    private static final FastThreadLocal<LoadingCache<Object, Reference>> THREAD_LOCAL = new FastThreadLocal<LoadingCache<Object, Reference>>() {
         @Override
-        protected LoadingCache<Object, IMutableReference<Optional<Object>>> initialValue() throws Exception {
-            final LoadingCache<Object, IMutableReference<Optional<Object>>> map = Caffeine.newBuilder()
+        protected LoadingCache<Object, Reference> initialValue() throws Exception {
+            final LoadingCache<Object, Reference> map = Caffeine.newBuilder()
                     .weakKeys()
-                    .<Object, IMutableReference<Optional<Object>>> build((key) -> {
-                        return new MutableReference<Optional<Object>>();
+                    .removalListener(
+                            (@Nullable final Object key, @Nullable final Reference value, final RemovalCause cause) -> {
+                                WeakThreadLocalReference.onRemoval(value);
+                            })
+                    .<Object, Reference> build((key) -> {
+                        return new Reference();
                     });
             return map;
         }
+
+        @Override
+        protected void onRemoval(final com.github.benmanes.caffeine.cache.LoadingCache<Object, Reference> value)
+                throws Exception {
+            for (final Reference reference : value.asMap().values()) {
+                WeakThreadLocalReference.onRemoval(reference);
+            }
+        };
     };
 
     private final Object key = new Object();
+
+    private static void onRemoval(final Reference reference) {
+        if (reference == null) {
+            return;
+        }
+        reference.close();
+    }
 
     protected V initialValue() {
         return null;
@@ -40,26 +64,26 @@ public class WeakThreadLocalReference<V> implements IMutableReference<V> {
     @SuppressWarnings("unchecked")
     @Override
     public V get() {
-        final LoadingCache<Object, IMutableReference<Optional<Object>>> map = THREAD_LOCAL.get();
-        final IMutableReference<Optional<Object>> reference = map.get(key);
+        final LoadingCache<Object, Reference> map = THREAD_LOCAL.get();
+        final Reference reference = map.get(key);
         final Optional<V> optional = (Optional<V>) reference.get();
         if (optional != null) {
             return optional.orElse(null);
         } else {
             final V initialValue = initialValue();
-            reference.set(Optional.ofNullable(initialValue));
+            reference.set(initialValue, isCloseOnRemoval());
             return initialValue;
         }
     }
 
     @Override
     public void set(final V value) {
-        final LoadingCache<Object, IMutableReference<Optional<Object>>> map = THREAD_LOCAL.get();
+        final LoadingCache<Object, Reference> map = THREAD_LOCAL.get();
         if (value == null) {
             map.invalidate(key);
         } else {
-            final IMutableReference<Optional<Object>> reference = map.get(key);
-            reference.set(Optional.ofNullable(value));
+            final Reference reference = map.get(key);
+            reference.set(value, isCloseOnRemoval());
         }
     }
 
@@ -71,8 +95,41 @@ public class WeakThreadLocalReference<V> implements IMutableReference<V> {
     }
 
     public void remove() {
-        final LoadingCache<Object, IMutableReference<Optional<Object>>> map = THREAD_LOCAL.get();
+        final LoadingCache<Object, Reference> map = THREAD_LOCAL.get();
         map.invalidate(key);
+    }
+
+    protected boolean isCloseOnRemoval() {
+        return false;
+    }
+
+    private static final class Reference implements ISafeCloseable {
+
+        private Object value;
+        private boolean closeOnRemoval;
+
+        public void set(final Object value, final boolean closeOnRemoval) {
+            this.value = value;
+            this.closeOnRemoval = closeOnRemoval;
+        }
+
+        public Object get() {
+            return value;
+        }
+
+        @Override
+        public void close() {
+            if (!closeOnRemoval) {
+                return;
+            }
+            final Object valueCopy = value;
+            if (valueCopy != null) {
+                Closeables.close(valueCopy);
+                value = null;
+            }
+            closeOnRemoval = false;
+        }
+
     }
 
 }
