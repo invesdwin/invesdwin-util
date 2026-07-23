dfac48d4f42f07bf63fa16e5a9e60cd3e3cdf207
Edwin Stang <gsubes@gmail.com>
2026-07-23 13:34:55 +0200

    use package private method instead of methodHandle reflections

--- invesdwin-util-parent/invesdwin-util/src/main/java/org/joda/time/format/InternalParserAccessor.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/org/joda/time/format/InternalParserAccessor.java
@@ -1,37 +1,14 @@
 package org.joda.time.format;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.reflect.Field;
-
 import javax.annotation.concurrent.Immutable;
-
-import de.invesdwin.util.lang.reflection.Reflections;
 
 @Immutable
 public class InternalParserAccessor {
 
-    private static final MethodHandle MH_DATETIMEFORMATTER_IPARSER;
-
-    static {
-        final Field iParserField = Reflections.findField(DateTimeFormatter.class, "iParser");
-        Reflections.makeAccessible(iParserField);
-        final Lookup lookup = MethodHandles.lookup();
-        try {
-            MH_DATETIMEFORMATTER_IPARSER = lookup.unreflectGetter(iParserField);
-        } catch (final IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
     private final InternalParser parser;
 
     public InternalParserAccessor(final DateTimeFormatter formatter) {
-        try {
-            this.parser = (InternalParser) MH_DATETIMEFORMATTER_IPARSER.invoke(formatter);
-        } catch (final Throwable e) {
-            throw new RuntimeException(e);
-        }
+        this.parser = formatter.getParser0();
     }
 
     public InternalParserAccessor(final InternalParser parser) {
--- invesdwin-util-parent/invesdwin-util/src/main/java/org/joda/time/format/InternalPrinterAccessor.java
+++ invesdwin-util-parent/invesdwin-util/src/main/java/org/joda/time/format/InternalPrinterAccessor.java
@@ -1,10 +1,6 @@
 package org.joda.time.format;
 
 import java.io.IOException;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.reflect.Field;
 import java.util.Locale;
 
 import javax.annotation.concurrent.Immutable;
@@ -13,31 +9,13 @@ import org.joda.time.Chronology;
 import org.joda.time.DateTimeZone;
 import org.joda.time.ReadablePartial;
 
-import de.invesdwin.util.lang.reflection.Reflections;
-
 @Immutable
 public class InternalPrinterAccessor {
 
-    private static final MethodHandle MH_DATETIMEFORMATTER_IPRINTER;
-
-    static {
-        final Field iPrinterField = Reflections.findField(DateTimeFormatter.class, "iPrinter");
-        Reflections.makeAccessible(iPrinterField);
-        final Lookup lookup = MethodHandles.lookup();
-        try {
-            MH_DATETIMEFORMATTER_IPRINTER = lookup.unreflectGetter(iPrinterField);
-        } catch (final IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
     private final InternalPrinter printer;
 
     public InternalPrinterAccessor(final DateTimeFormatter formatter) {
-        try {
-            this.printer = (InternalPrinter) MH_DATETIMEFORMATTER_IPRINTER.invoke(formatter);
-        } catch (final Throwable e) {
-            throw new RuntimeException(e);
-        }
+        this.printer = formatter.getPrinter0();
     }
 
     public InternalPrinterAccessor(final InternalPrinter printer) {
